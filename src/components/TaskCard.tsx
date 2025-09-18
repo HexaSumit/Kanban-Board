12f1f3732c4dd00f583aeb33cdbdd26c180d6098
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-18 15:45:01 +0530

    style: make TaskCard smaller and update text sizes accordingly

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -3,21 +3,22 @@ import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 
 const TaskCard = () => {
     return (
-        <div className=" mb-4 bg-green-300 border border-gray-300 rounded-2xl w-76 p-5 flex flex-col gap-4">
-            <div className=" flex items-center justify-between">
-                <div className=" flex items-center justify-center px-3 rounded-xl bg-green-100 text-green-600">
-                    <span><MdKeyboardDoubleArrowUp /></span>
-                    <span>low</span>
+        <div className="mb-4 bg-green-300 border border-gray-300 rounded-2xl w-64 p-3 flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center justify-center px-2 py-1 rounded-lg bg-green-100 text-green-600 text-xs">
+                    <span><MdKeyboardDoubleArrowUp size={14} /></span>
+                    <span className="ml-1">low</span>
                 </div>
-                <span ><IoIosMore size={20} /></span>
+                <span><IoIosMore size={16} /></span>
             </div>
             <div>
-                <h2 className=" text-xl font-bold">Design Mockups</h2>
+                <h2 className="text-lg font-bold">Design Mockups</h2>
             </div>
             <div>
-                <p className=" text-md text-gray-500">Create initial Design Drafts</p>
+                <p className="text-sm text-gray-500">Create initial Design Drafts</p>
             </div>
         </div>
+
     )
 }
 
